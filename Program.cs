dfba38e78e549fc59485cfde19d8e7bcccb860e9
Yannohkym <iankym1997@gmail.com>
2025-06-17 11:18:38 +0300

    few changes  dbcontext

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using InsurancePolicies.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 namespace InsurancePolicies
 {
     public class Program
@@ -11,7 +12,10 @@ namespace InsurancePolicies
             // Add services to the container.
 
             builder.Services.AddControllers();
-          
+           
+            //builder.Services.Configure<PositionOptions>(
+            //  builder.Configuration.GetSection(PositionOptions.Position));
+
             builder.Services.AddDbContext<AppDbContext>(options =>
                      options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
                  // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
